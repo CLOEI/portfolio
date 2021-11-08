cf533c465d808f7248b9b4bf8fa5173e53e3abaa
Cendy <cendyemail@gmail.com>
2021-11-08 18:00:59 +0700

    small change to animation

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,9 +1,38 @@
 import Image from 'next/image';
 import style from '../styles/components/projectcard.module.scss';
+import { useInView } from 'react-intersection-observer';
+import { useAnimation, motion } from 'framer-motion';
+import { useEffect } from 'react';
 
 function ProjectCard({ imageURL, title, desc, alt }) {
+	const controls = useAnimation();
+	const [ref, inView] = useInView({ threshold: 0.4 });
+	const cardVariant = {
+		hidden: { opacity: 0, y: 100 },
+		show: {
+			opacity: 1,
+			y: 0,
+			transition: {
+				ease: 'easeOut',
+				durations: 2,
+			},
+		},
+	};
+
+	useEffect(() => {
+		if (inView) {
+			controls.start('show');
+		}
+	}, [controls, inView]);
+
 	return (
-		<div className={style.container}>
+		<motion.div
+			className={style.container}
+			variants={cardVariant}
+			initial="hidden"
+			animate={controls}
+			ref={ref}
+		>
 			<div className={style.image}>
 				<Image
 					src={imageURL}
@@ -17,7 +46,7 @@ function ProjectCard({ imageURL, title, desc, alt }) {
 				<p>{title}</p>
 				<p>{desc}</p>
 			</div>
-		</div>
+		</motion.div>
 	);
 }
 
