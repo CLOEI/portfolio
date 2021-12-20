b07b40b4634587c1ee119240c81ff96836c15bd6
Cendy <cendyemail@gmail.com>
2021-12-20 13:19:18 +0700

    Initialize style for the post

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,20 +1,42 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { useRouter } from 'next/router';
+import { BLOCKS } from '@contentful/rich-text-types';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import Head from 'next/head';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+const options = {
+	renderNode: {
+		[BLOCKS.PARAGRAPH]: (node, children) => <Text weight="400">{children}</Text>,
+		[BLOCKS.HEADING_2]: (node, children) => <Heading as="h2">{children}</Heading>,
+	},
+};
 
 function Blog({ post }) {
+	const body = post[0].fields.body;
+	const title = post[0].fields.title;
+	const hero = post[0].fields.image?.fields?.file?.url || null;
+
 	const router = useRouter();
-	console.log(post);
+
 	return (
-		<>
+		<Wrapper>
 			<Head>
-				<title>Cendy • {post[0].fields.title}</title>
+				<title>Cendy • {title}</title>
 			</Head>
-			<div onClick={() => router.back()}>Temp button</div>
-			{documentToReactComponents(post[0].fields.body)}
-		</>
+			<BackButton onClick={() => router.back()} />
+			{hero && (
+				<ImageWrapper>
+					<Image src={hero} layput="fill" alt={title} objectFit="cover" />
+				</ImageWrapper>
+			)}
+			<Heading as="h1">{title}</Heading>
+			{documentToReactComponents(body, options)}
+		</Wrapper>
 	);
 }
 
@@ -46,5 +68,33 @@ export async function getStaticProps({ params }) {
 		},
 	};
 }
+
+const Text = styled.p`
+	font-family: 'Inter', sans-serif;
+	font-weight: ${(props) => props.weight || 800};
+	opacity: 87%;
+	line-height: 24px;
+`;
+const Heading = styled(Text)`
+	line-height: 28px;
+`;
+const BackButton = styled(FiArrowLeft)`
+	width: 40px;
+	height: 40px;
+	cursor: pointer;
+`;
+const Wrapper = styled.div`
+	margin: 10px;
+	@media (min-width: 768px) {
+		margin: 20px 20px 0 20px;
+	}
+`;
+const ImageWrapper = styled.div`
+	position: relative;
+	width: 100%;
+	padding-top: 42%;
+	margin-top: 10px;
+	background-color: ${(props) => props.theme['01dp']};
+`;
 
 export default Blog;
